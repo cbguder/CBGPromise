0ebae282969bf598cf4e15574e1570eda9f4e1ed
Can Berk Güder <cbg@cbg.me>
2015-07-12 15:40:58 -0700

    Initial commit.

---
+++ CBGPromise/CBGPromise.h
@@ -0,0 +1,19 @@
+//
+//  CBGPromise.h
+//  CBGPromise
+//
+//  Created by Can Berk Güder on 7/12/15.
+//  Copyright © 2015 CBG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CBGPromise.
+FOUNDATION_EXPORT double CBGPromiseVersionNumber;
+
+//! Project version string for CBGPromise.
+FOUNDATION_EXPORT const unsigned char CBGPromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBGPromise/PublicHeader.h>
+
+

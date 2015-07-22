7e55de14e7ebaffc8656f2f76b32042258e0126e
Can Berk Güder <cbg@cbg.me>
2015-07-21 19:46:05 -0700

    Call success/failure callbacks when promise is resolved before they are registered

--- CBGPromise/Future.swift
+++ CBGPromise/Future.swift
@@ -4,21 +4,36 @@ public class Future<T> {
     var successCallback: ((T) -> ())?
     var errorCallback: ((NSError) -> ())?
 
+    public var value: T?
+    public var error: NSError?
+
     public func then(callback: (T) -> ()) {
         successCallback = callback
+
+        if let value = value {
+            callback(value)
+        }
     }
 
     public func error(callback: (NSError) -> ()) {
         errorCallback = callback
+
+        if let error = error {
+            callback(error)
+        }
     }
 
     func resolve(value: T) {
+        self.value = value
+
         if let successCallback = successCallback {
             successCallback(value)
         }
     }
 
     func reject(error: NSError) {
+        self.error = error
+
         if let errorCallback = errorCallback {
             errorCallback(error)
         }
--- CBGPromiseTests/PromiseSpec.swift
+++ CBGPromiseTests/PromiseSpec.swift
@@ -12,27 +12,58 @@ class PromiseSpec: QuickSpec {
             beforeEach {
                 subject = Promise<String>()
 
-                subject.future.then { v in
-                    value = v
+                value = nil
+                error = nil
+            }
+
+            context("when the callbacks are registered before the promise is resolved/rejected") {
+                beforeEach {
+                    subject.future.then { v in
+                        value = v
+                    }
+
+                    subject.future.error { e in
+                        error = e
+                    }
                 }
 
-                subject.future.error { e in
-                    error = e
+                it("should resolve its future") {
+                    subject.resolve("My Special Value")
+
+                    expect(value).to(equal("My Special Value"))
+                }
+
+                it("should reject its future") {
+                    let expectedError = NSError(domain: "My Special Domain", code: 123, userInfo: nil)
+
+                    subject.reject(expectedError)
+
+                    expect(error).to(equal(expectedError))
                 }
             }
 
-            it("should resolve its future") {
-                subject.resolve("My Special Value")
+            context("when the callbacks are registered after the promise is resolved/rejected") {
+                it("should resolve its future") {
+                    subject.resolve("My Special Value")
 
-                expect(value).to(equal("My Special Value"))
-            }
+                    subject.future.then { v in
+                        value = v
+                    }
 
-            it("should reject its future") {
-                let expectedError = NSError(domain: "My Special Domain", code: 123, userInfo: nil)
+                    expect(value).to(equal("My Special Value"))
+                }
 
-                subject.reject(expectedError)
+                it("should reject its future") {
+                    let expectedError = NSError(domain: "My Special Domain", code: 123, userInfo: nil)
 
-                expect(error).to(equal(expectedError))
+                    subject.reject(expectedError)
+
+                    subject.future.error { e in
+                        error = e
+                    }
+
+                    expect(error).to(equal(expectedError))
+                }
             }
         }
     }

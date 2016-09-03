43ba76f613a869589f7f6c48e53b41496fab1249
Rachel Brindle <you@subluminal.net>
2016-09-03 09:31:22 -0700

    Update for swift 3 (xcode 8, beta 6)
    - Makes Promise and Future final classes (unable to be subclassed)
    - closure arguments are marked as escaping
    - Future.value is now read-only for users
    - Swift3-ify the libdispatch api calls
    - Remove warnings from unused function return values

--- CBGPromise/Promise.swift
+++ CBGPromise/Promise.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-public class Promise<T> {
+public final class Promise<T> {
     public let future: Future<T>
 
     public init() {
         future = Future<T>()
     }
 
-    public func resolve(value: T) {
+    public func resolve(_ value: T) {
         future.resolve(value)
     }
 }

b94affe6771602b82293080a3be59570fde8e965
Can Berk Güder <cbg@cbg.me>
2017-10-31 20:52:10 -0700

    Fix Swift Package Manager build

--- ci/swiftpm.sh
+++ ci/swiftpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mv Package.swift .Package.swift && cp .Package.test.swift Package.swift
-swift build --clean && swift build && swift test
+swift build && swift test
 RETVAL=$?
 mv .Package.swift Package.swift
 exit $RETVAL
